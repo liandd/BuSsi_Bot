1b1f9b8dca3aa8fd4c2fe951ad0f31d51855fbe8
liandd <juandavidtv24@gmail.com>
2024-11-14 15:45:12 -0500

    Speech format for Windows and Linux

--- controllers/chatbot_controller.py
+++ controllers/chatbot_controller.py
@@ -2,6 +2,7 @@
 
 import speech_recognition as sr
 import pyglet as pt
+import subprocess
 import pyaudio
 import time
 import sys
@@ -9,7 +10,6 @@ import os
 
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-
 from gtts import gTTS
 from pydub import AudioSegment
 from pathlib import Path
@@ -17,19 +17,37 @@ from models import chatbot_model as bussi
 from utils import recorder as rd
 
 # Globales
+#BASE_DIR = 'C:\\PROYECTO\\static'
+BASE_DIR = 'static'
+OGG_DIR = 'static/audios_ogg'
+WAV_DIR = 'static/audios_wav'
+MP3_DIR = 'static/audios_mp3'
+
 chatbot = bussi.Chatbot() 
 r = sr.Recognizer()
 
 class ChatbotControlador:
     def __init__(self):
-        self.audio_dir = os.path.join(os.path.dirname(__file__), '..','static')
+        #self.audio_dir = os.path.join(os.path.dirname(__file__), '..','static')
+        self.ogg_dir = OGG_DIR
+        self.wav_dir = WAV_DIR
+        self.mp3_dir = MP3_DIR
 
-    def speech_file(self):
-        ogg_file = os.path.join(self.audio_dir, 'audios_ogg', 'Hola_amigo.ogg')
-        wav_file = os.path.join(self.audio_dir, 'audios_wav', 'Prueba3.wav')
-
-        sound = AudioSegment.from_ogg(ogg_file)
-        sound.export(wav_file, format='wav')
+    def speech_file(self, key:bool):
+        ogg_file = os.path.join(self.ogg_dir, 'Hola_amigo.ogg')
+        wav_file = os.path.join(self.wav_dir, 'Prueba3.wav')
+        
+        if key:
+            #Convertir en windows
+            try:
+                command = ['ffmpeg','-y','-i',ogg_file, wav_file]
+                subprocess.run(command, check=True, stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+            except subprocess.CalledProcessError as e:
+                print("ERROR EN LA CONVERSION MP3 A WAV!", e)
+        else:
+            #Convertir en linux
+            sound = AudioSegment.from_ogg(ogg_file)
+            sound.export(wav_file, format='wav')
 
         audio_wav_file = sr.AudioFile(wav_file)
         with audio_wav_file as source:
@@ -38,20 +56,29 @@ class ChatbotControlador:
 
         return r.recognize_google(audio, language='es-ES')
 
-    def generate_audio(self, text):
-        mp3_path = os.path.join(self.audio_dir, 'audios_mp3', 'audio_test1.mp3')
-        wav_path = os.path.join(self.audio_dir, 'audios_wav', 'audio_test1.wav')
+    def generate_audio(self, text, key:bool):
+        mp3_path = os.path.join(self.mp3_dir, 'audio_test1.mp3')
+        wav_path = os.path.join(self.wav_dir, 'Prueba1.wav')
 
         tts = gTTS(text=text, lang='es')
         tts.save(mp3_path)
 
-        audio = AudioSegment.from_mp3(mp3_path)
-        audio.export(wav_path, format='wav')
+        if key:
+            #Convertir en windows
+            try:
+                command = ['ffmpeg','-y','-i', mp3_path, wav_path]
+                subprocess.run(command, check=True, stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+            except subprocess.CalledProcessError as e:
+                print("ERROR EN LA CONVERSION MP3 A WAV!", e)
+        else:
+            #Convertir en linux
+            sound = AudioSegment.from_mp3(mp3_path)
+            sound.export(wav_path, format='wav')
 
         return wav_path
 
     def start_chat(self):
-        initial_text = self.speech_file()
+        initial_text = self.speech_file(False)
         print(initial_text+'\n')
         self.play_audio(initial_text)
         #response = chatbot.get_response(str_de_audio)
@@ -76,12 +103,13 @@ class ChatbotControlador:
                     self.play_audio(str_response)
                     continue
                     
-                #initial_text = input().lower()
-                initial_text = self.record_linux()
+                initial_text = input().lower()
+                #initial_text = self.record_linux()
+                #print(initial_text+'\n')
 
     def play_audio(self, response):
         if response:
-            wav_path = self.generate_audio(response)
+            wav_path = self.generate_audio(response, False)
             music = pt.media.load(wav_path, streaming=False)
             music.play()
             time.sleep(music.duration)
@@ -98,7 +126,7 @@ class ChatbotControlador:
 
     def record_linux(self):
         rd.record_close()
-        wav_file = os.path.join(self.audio_dir, 'audios_wav', 'from_mic.wav')
+        wav_file = os.path.join(self.wav_dir, 'from_mic.wav')
 
         wav_file = sr.AudioFile(wav_file)
         with wav_file as source:
